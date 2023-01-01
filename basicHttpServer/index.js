3423369441daf2d3780df50e54612b3c0325b131
AvinashPandey2000 <avinash201pandey@gmail.com>
2023-01-01 15:32:15 +0530

    call html file via server

--- basicHttpServer/index.js
+++ basicHttpServer/index.js
@@ -1,12 +1,23 @@
-const http=require('http')
+const http=require('http')     //http ki method use  kr ne k liye
 const port=8000;
+const fs=require('fs');        // html file use kr ne k liye
 
 //step 2:  pass the handeler to the server
 function requestHandler(req,res){
     console.log(`url : ${req.url}`);
 
+    // send the response
+    res.writeHead(200,{'content-type':'text/html'});
+
+    fs.readFile('./index.html', function(err,data){        // data k under index.html ka all data aayega
+        if(err){
+            console.log(`Error !  ${err}`);
+          return  res.end(`<h1> Error !  ${err} </h1>`);
+        }
+
+        return res.end(data);    // data ko how kr ne k liye
+    });
     
-    res.end('response end !');
 }
 
 
